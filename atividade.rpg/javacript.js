1476edfeb36074d8a652519a595d1436b5928136
Gustavo4157 <larissa.r.lopes7@aluno.senai.br>
2026-05-19 12:04:20 -0300

    .
    .

---
+++ atividade.rpg/javacript.js
@@ -0,0 +1,21 @@
+class Personagem {
+    constructor(nome, titulo, hp, mana, energia) {
+        this.nome = nome;
+        this.titulo = titulo;
+        this.hp = hp;
+        this.mana = mana;
+        this.energia = energia;
+    }
+}
+
+class Habilidade {
+    constructor(id, nome, dano, custo, energia) {
+        this.id = id;
+        this.nome = nome;
+        this.dano = dano;
+        this.custo = custo;
+        this.energia = energia;
+    }
+}
+// Instancia Classes - Criar Objetos
+let hero = new Personagem("R-01","🐶 cachorro", 100, 100, 0);
